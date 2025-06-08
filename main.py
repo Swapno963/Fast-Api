0e570a5e447849511ba5d33cafbbc2f8e53a6481
swapno963 <swapno963@gmail.com>
2025-06-08 11:58:25 +0600

    added /about

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def index():
+async def index() -> dict[str, str]:
     return {"message": "hello this is swapno"}
+
+
+
+@app.get("/about")
+async def about() -> str:
+    return "This is a simple FastAPI application."
