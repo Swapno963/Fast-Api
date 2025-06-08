5994c0f0c3f3d93f82a7a960fe8e5c571076dbab
swapno963 <swapno963@gmail.com>
2025-06-08 13:36:57 +0600

    Worked with multiple query parameter

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = FastAPI()
 
 BAND = [
     {"id": 1, "name": "Band A", "genre": "Rock"},
+    {"id": 6, "name": "Band B", "genre": "Pop"},
     {"id": 2, "name": "Band B", "genre": "Pop", "albutms": [
         {"title": "Album 1", "release_date": "2020-03-24"}
         ]
@@ -14,8 +15,29 @@ BAND = [
 ]
 
 @app.get("/bands")
-async def index() -> list[Band]:
-    return [Band(**b) for b in BAND]
+async def index(
+    genre: GenreUrlChoices | None = None,
+    has_albums: bool = False
+    ) -> list[Band]:
+    
+    bands_list = [Band(**b) for b in BAND]
+    if genre:
+        filtered_bands = [b for b in bands_list if b.genre.value.lower() == genre.value.lower()]
+        print(f"Genre: {genre}, Filtered bands: {filtered_bands}")  # Debugging line
+        if not filtered_bands:
+            raise HTTPException(status_code=404, detail="No bands found for this genre")
+        bands_list = filtered_bands
+
+
+    if has_albums:
+        filtered_bands = [b for b in bands_list if len(b.albutms) > 0]
+    else:
+        filtered_bands = [b for b in bands_list if len(b.albutms) == 0]
+
+        if not filtered_bands:
+            raise HTTPException(status_code=404, detail="No bands with albums found")
+        bands_list = filtered_bands
+    return bands_list
 
 
 
